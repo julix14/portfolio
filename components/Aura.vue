2cfd42b7c90e5b7b7fadb57ed10f131253638988
Juliusbecker <julix.becker@gmail.com>
2023-09-13 11:31:21 +0200

    Fix Aura

--- components/Aura.vue
+++ components/Aura.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="cursor-aura pointer-events-none fixed top-0 right-0 bottom-0 left-0 z-30 transition-all duration-300 lg:absolute"
+    class="pointer-events-none fixed top-0 right-0 bottom-0 left-0 z-30 transition-all duration-300"
     :style="{
       background:
         'radial-gradient(150px at ' +
@@ -8,21 +8,20 @@
         'px ' +
         cursorY +
         'px, rgba(29, 78, 216, 0.15), transparent 80%)',
-    }"
-  ></div>
+    }"></div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-let cursorX = ref(0);
-let cursorY = ref(0);
+  import { ref, onMounted } from "vue";
+  let cursorX = ref(0);
+  let cursorY = ref(0);
 
-onMounted(() => {
-  window.addEventListener("mousemove", updateCursorPosition);
-});
+  onMounted(() => {
+    window.addEventListener("mousemove", updateCursorPosition);
+  });
 
-function updateCursorPosition(event) {
-  cursorX.value = event.clientX;
-  cursorY.value = event.clientY;
-}
+  function updateCursorPosition(event) {
+    cursorX.value = event.clientX;
+    cursorY.value = event.clientY;
+  }
 </script>
